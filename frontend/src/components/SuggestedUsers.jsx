38e977003f19de2d0130b3430aef5bb95ae09a0e
unknown <toan9370@donga.edu.vn>
2025-09-09 22:32:22 +0700

    fix-bug-map

--- frontend/src/components/SuggestedUsers.jsx
+++ frontend/src/components/SuggestedUsers.jsx
@@ -12,36 +12,35 @@ export default function SuggestedUsers() {
         <span className="font-medium cursor-pointer">See All</span>
       </div>
       <div className="flex gap-3 flex-col my-4">
-        {suggestedUsers &&
-          suggestedUsers.map((user) => {
-            return (
-              <div key={user._id} className="flex justify-between items-center">
-                <div className="flex gap-3 items-center">
-                  <Link to={`/profile/${user?._id}`}>
-                    <Avatar>
-                      <AvatarImage
-                        className="object-cover"
-                        src={user?.profilePicture}
-                        alt="post_image"
-                      />
-                      <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                  </Link>
-                  <div className="flex flex-col gap-1 font-bold text-sm">
-                    <h1 className="font-semibold text-sm">
-                      <Link to={`/profile/${user?._id}`}>{user?.username}</Link>
-                    </h1>
-                    <span className="font-normal text-gray-600 text-sm">
-                      {user?.bio || "Bio here..."}
-                    </span>
-                  </div>
-                </div>
-                <div className="text-[#3BADF8] text-xs font-bold cursor-pointer hover:text-[#3495d6] pl-2">
-                  Follow
+        {suggestedUsers.map((user) => {
+          return (
+            <div key={user._id} className="flex justify-between items-center">
+              <div className="flex gap-3 items-center">
+                <Link to={`/profile/${user?._id}`}>
+                  <Avatar>
+                    <AvatarImage
+                      className="object-cover"
+                      src={user?.profilePicture}
+                      alt="post_image"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </Link>
+                <div className="flex flex-col gap-1 font-bold text-sm">
+                  <h1 className="font-semibold text-sm">
+                    <Link to={`/profile/${user?._id}`}>{user?.username}</Link>
+                  </h1>
+                  <span className="font-normal text-gray-600 text-sm">
+                    {user?.bio || "Bio here..."}
+                  </span>
                 </div>
               </div>
-            );
-          })}
+              <div className="text-[#3BADF8] text-xs font-bold cursor-pointer hover:text-[#3495d6] pl-2">
+                Follow
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
